9175d1bd2af0ef6a0044e913a7efedae7d0aa563
Cole Webb <colewebb@users.noreply.github.com>
2016-09-23 16:22:02 -0700

    deleting for gh-pages

--- apps/search/whitepages.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-search = raw_input("Enter a 9-digit phone number with hyphens, such as 253-845-9920: ")
-search="firefox http://www.whitepages.com/phone/1-" + search
-subprocess.call(search, shell=True)
