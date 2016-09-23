a6a9435d3dff3e5c06d0abde7cdbd73aeb88c0d5
Cole Webb <colewebb@users.noreply.github.com>
2016-09-23 16:22:15 -0700

    deleting for gh-pages

--- apps/search/wikipedia.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-input = raw_input("What do you want to search Wikipedia for? ")
-execute = "firefox https://en.wikipedia.org/wiki/Special:Search?search='" + input + "'"
-subprocess.call(execute, shell=True)
