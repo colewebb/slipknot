e2464cdd894f0e152aa5c866ba82f45a10b90ac3
Cole Webb <colewebb@users.noreply.github.com>
2016-09-23 16:20:37 -0700

    deleting for gh-pages

--- apps/search/google.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-input = raw_input("What would you like to search Google for? ")
-query = "firefox 'https://www.google.com/search?client=ubuntu&channel=fs&q=" + input + "&ie=utf-8&oe=utf-8'"
-subprocess.call(query, shell=True)
